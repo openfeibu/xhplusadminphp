02874ee59d377137bfa617d2e7b85cf4d864d7db
xza <xza@2199d3b0-2f51-244a-aa9d-ac244ff575e0>
2016-11-12 04:32:41 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhplusadminphp@17 2199d3b0-2f51-244a-aa9d-ac244ff575e0

--- branches/app/Repositories/AssociationRepositoryEloquent.php
+++ branches/app/Repositories/AssociationRepositoryEloquent.php
@@ -7,6 +7,7 @@ use Prettus\Repository\Criteria\RequestCriteria;
 use App\Repositories\AssociationRepository;
 use App\Association;
 use App\Association_label;
+use DB;
 
 /**
  * Class AdminUserRepositoryEloquent
@@ -25,7 +26,7 @@ class AssociationRepositoryEloquent extends BaseRepository implements Associatio
     }
 
     public function getAssociationAll(){
-        $associations = Association::orderBy('aid', 'desc')->paginate(config('admin_config.page'));
+        $associations = Association::orderBy('sort', 'desc')->paginate(config('admin_config.page'));
         return $associations;
     }
 
@@ -37,8 +38,24 @@ class AssociationRepositoryEloquent extends BaseRepository implements Associatio
     public function createAssociation(){
         
     }
+
     public function getAccusationLabel(){
         return Association_label::get();
+    }
+
+    public function getMaxSort(){
+        return Association::select(DB::raw('max(sort) as max_sort'))
+                            ->first();
+    }
+    public function updateAssociationSort($max_sort,$aid){
+        $update = Association::where('aid',$aid)->update([
+                'sort' => $max_sort+1,
+            ]);
+        if($update){
+            return 200;
+        }else{
+            return 403;
+        }
     }
 
 }
