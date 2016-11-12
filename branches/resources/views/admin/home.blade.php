5b0d6adc1b3843b3a6319b25b11275a5098a4efa
xza <xza@2199d3b0-2f51-244a-aa9d-ac244ff575e0>
2016-11-12 06:33:27 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhplusadminphp@21 2199d3b0-2f51-244a-aa9d-ac244ff575e0

--- branches/resources/views/admin/home.blade.php
+++ branches/resources/views/admin/home.blade.php
@@ -5,10 +5,6 @@
     <div class="pageheader">
         <h2><i class="fa fa-home"></i> Dashboard <span>Subtitle goes here...</span></h2>
         <div class="breadcrumb-wrapper">
-            <span class="label">You are here:</span>
-            <ol class="breadcrumb">
-                <li class="active">Dashboard</li>
-            </ol>
         </div>
     </div>
 
