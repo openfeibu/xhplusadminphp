c0a15ba36ba9602396d4658d24936af3f123c414
wzj <wzj@2199d3b0-2f51-244a-aa9d-ac244ff575e0>
2017-03-21 16:15:57 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhplusadminphp@85 2199d3b0-2f51-244a-aa9d-ac244ff575e0

--- branches/app/Http/Controllers/Admin/HomeController.php
+++ branches/app/Http/Controllers/Admin/HomeController.php
@@ -30,9 +30,9 @@ class HomeController extends BaseController
     public function index(Request $request)
     {
 
-		$beginThisweek=mktime(0,0,0,date('m'),date('d')-date('w')+1,date('Y'));
+		$beginThisweek = mktime(0,0,0,date('m'),date('d')-date('w')+1,date('Y'));
 
-		$endThisweek=mktime(23,59,59,date('m'),date('d')-date('w')+7,date('Y'));
+		$endThisweek = mktime(23,59,59,date('m'),date('d')-date('w')+7,date('Y'));
 	
 	    $data['active_user_this_week_count'] = User::whereBetween('last_visit',[dtime($beginThisweek),dtime($endThisweek)])->count();
 
@@ -42,11 +42,11 @@ class HomeController extends BaseController
 
 	    $data['order_this_week_count'] = Order::whereBetween('created_at',[dtime($beginThisweek),dtime($endThisweek)])->where('status','completed')->count();
 
-	    
+	   	
 
-		$beginThismonth=mktime(0,0,0,date('m'),1,date('Y'));
+		$beginThismonth = mktime(0,0,0,date('m'),1,date('Y'));
 
-		$endThismonth=mktime(23,59,59,date('m'),date('t'),date('Y'));
+		$endThismonth = mktime(23,59,59,date('m'),date('t'),date('Y'));
 
 		$data['active_user_this_month_count'] = User::whereBetween('last_visit',[dtime($beginThismonth),dtime($endThismonth)])->count();
 
@@ -55,6 +55,12 @@ class HomeController extends BaseController
 		$data['order_info_this_month_count'] = OrderInfo::whereBetween('created_at',[dtime($beginThismonth),dtime($endThismonth)])->where('order_status',2)->count();
 
 		$data['order_this_month_count'] = Order::whereBetween('created_at',[dtime($beginThismonth),dtime($endThismonth)])->where('status','completed')->count();
+
+		$data['wallet'] = User::select(DB::raw('SUM(wallet) as wallet'))->value('wallet');
+
+		$data['service_fee'] = Order::select(DB::raw('SUM(service_fee) as service_fee'))->where('status','completed')->value('service_fee');
+
+		$data['shop_trade'] = Shop::select(DB::raw('SUM(income) as income'))->value('income');
     
         return view('admin.home')->with('data',$data);
     }
