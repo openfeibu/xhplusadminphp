0c0bec334b1fca5ab57cbcc675c2d7bca4634f08
wzj <wzj@2199d3b0-2f51-244a-aa9d-ac244ff575e0>
2017-09-11 04:05:15 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhplusadminphp@178 2199d3b0-2f51-244a-aa9d-ac244ff575e0

--- branches/resources/views/admin/telecom/enrollments.blade.php
+++ branches/resources/views/admin/telecom/enrollments.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.admin-app')
 
 @section('content')
+    <link rel="stylesheet" href="{{ asset('css/rome.css') }}" type="" media=""/>
     <div class="pageheader">
         <h2><i class="fa fa-home"></i> Dashboard <span>系统设置</span></h2>
         {!! Breadcrumbs::render('admin-telecomEnroll-enrollments') !!}
@@ -16,7 +17,10 @@
                     <div class="panel-body">
 
                         <div class="pull-right">
+                            <div class="btn-group mr10">
 
+								<a  data-target="#export" data-toggle="modal" class="btn btn-white tooltips" data-original-title="导出"  ><i class="glyphicon glyphicon-save"></i></a>
+                            </div>
                         </div><!-- pull-right -->
 
                         <h5 class="subtitle mb5">预约列表</h5>
@@ -61,11 +65,35 @@
         </div><!-- row -->
 
     </div>
+    <form action="{{ route('admin.telecomEnroll.saveEnroll') }}" method="get">
+	    <div class="modal fade bs-modal-lg" id="export" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+		  <div class="modal-dialog modal-lg">
+		    <div class="modal-content">
+			    <div class="modal-header">
+				    <div class="form-group">
+					    <label for="date" class="col-sm-2 control-label">开始时间</label>
+					    <div class="col-sm-10">
+					      <input type="text" class="form-control"  name="date" id="date" value="{{$date}}">
+					    </div>
+					</div>
+				</div>
+		      	<div class="modal-footer">
+	                <input type="submit" value="确定" class="btn btn-default" id="submit" >
+	                <button type="button" class="btn btn-default" data-dismiss="modal">关闭
+	                </button>
+	            </div>
+		    </div>
+		  </div>
+		</div>
+	</form>
 @endsection
 
 @section('javascript')
     @parent
+    <script src="{{ asset('js/rome.min.js') }}"></script>
     <script src="{{ asset('js/ajax.js') }}"></script>
-    
+    <script type="text/javascript">
+        rome(date, { time: false });
+    </script>
 
 @endsection
