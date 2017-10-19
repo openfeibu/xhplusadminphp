3cbccf3f87aa677767c2a08b00127773fc455814
wzj <wzj@2199d3b0-2f51-244a-aa9d-ac244ff575e0>
2017-10-19 10:08:40 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhplusadminphp@205 2199d3b0-2f51-244a-aa9d-ac244ff575e0

--- branches/app/Http/Controllers/Admin/OrderInfoController.php
+++ branches/app/Http/Controllers/Admin/OrderInfoController.php
@@ -49,17 +49,33 @@ class OrderInfoController extends BaseController
 		});
 		$dateym = isset($request->dateym) && $request->dateym ? $request->dateym : date('Y-m');
 		$ranks = app('orderInfoRepositoryEloquent')->getMonthDayRank($dateym);
-		return view('admin.order_info.canteen_month_day_rank', compact('ranks','dateym'));
+		$count_sum = $seller_shipping_fee_sum = $shipping_fee_sum = $goods_amount_sum = 0;
+		foreach ($ranks as $key => $rank) {
+			$count_sum += $rank->count;
+			$seller_shipping_fee_sum += $rank->seller_shipping_fee;
+			$shipping_fee_sum += $rank->shipping_fee;
+			$goods_amount_sum += $rank->goods_amount;
+		}
+		return view('admin.order_info.canteen_month_day_rank', compact('ranks','dateym','count_sum','seller_shipping_fee_sum','shipping_fee_sum','goods_amount_sum'));
 	}
 	public function monthDayRankDownload(Request $request)
 	{
 		$dateym = isset($request->dateym) && $request->dateym ? $request->dateym : date('Y-m');
 		$ranks = app('orderInfoRepositoryEloquent')->monthDayRankDownload($dateym);
+		$count_sum = $seller_shipping_fee_sum = $shipping_fee_sum = $goods_amount_sum = 0;
+		foreach ($ranks as $key => $rank) {
+			$count_sum += $rank->订单量;
+			$goods_amount_sum += $rank->订单总商品额;
+			$seller_shipping_fee_sum += $rank->商家出总运费;
+			$shipping_fee_sum += $rank->买家出总运费;
+		}
+		$ranks = $ranks ? $ranks->toArray() : [];
+		$ranks = array_merge($ranks,[['日期' => '','订单量' => $count_sum,'订单总商品额' => $goods_amount_sum.'元','商家出总运费' => $seller_shipping_fee_sum.'元','买家出总运费' => $shipping_fee_sum.'元']]);
 		$name = $request->dateym.'饭堂每天销量列表';
 		Excel::create($name,function($excel) use ($ranks){
-		  $excel->sheet('score', function($sheet) use ($ranks){
-			$sheet->fromArray($ranks);
-		  });
+		  	$excel->sheet('score', function($sheet) use ($ranks){
+				$sheet->fromArray($ranks);
+		  	});
 		})->export('xls');
 	}
 }
