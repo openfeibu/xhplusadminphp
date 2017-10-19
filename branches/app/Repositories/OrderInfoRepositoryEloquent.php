125ed25991971ef11a7e014d600d2532fa78de88
wzj <wzj@2199d3b0-2f51-244a-aa9d-ac244ff575e0>
2017-10-19 09:01:24 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhplusadminphp@198 2199d3b0-2f51-244a-aa9d-ac244ff575e0

--- branches/app/Repositories/OrderInfoRepositoryEloquent.php
+++ branches/app/Repositories/OrderInfoRepositoryEloquent.php
@@ -96,8 +96,10 @@ class OrderInfoRepositoryEloquent extends BaseRepository implements OrderInfoRep
     {
         $ranks = OrderInfo::select(DB::raw("count('*') as count,DATE_FORMAT(order_info.created_at,'%Y-%m-%d') as datemd,SUM(order_info.seller_shipping_fee) as seller_shipping_fee ,SUM(order_info.seller_shipping_fee) as seller_shipping_fee,SUM(order_info.shipping_fee) as shipping_fee,SUM(order_info.goods_amount) as goods_amount"))
                         ->join('order','order.order_id','=','order_info.order_id')
+                        ->join('shop','shop.shop_id','=','order_info.shop_id')
                         ->whereRaw("DATE_FORMAT(order_info.created_at,'%Y-%m') = '".$datemd."' ")
                         ->where('order_info.order_status','2')
+                        ->where('shop.shop_type',3)
                         ->orderBy('datemd','asc')
                         ->groupBy('datemd')
                         ->get();
